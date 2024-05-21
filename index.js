580177857a31cedc2251c701d536652f9071be83
saadafahmed45 <hafijnahid45@.com>
2024-05-22 03:21:43 +0600

    added

--- index.js
+++ index.js
@@ -1,14 +1,135 @@
 const express = require("express");
 const app = express();
-const port = 5000;
-const blogs =
-  "This app starts a server and listens on port 3000 for connections. The app responds with “Hello World!” for requests to the root URL (/) or route. For every other path, it will respond with a 404 Not Found.";
-app.get("/", (req, res) => {
-  res.send("This is Devs-Bloger site Database");
+const cors = require("cors");
+require("dotenv").config();
+const port = 7000;
+// middelware
+app.use(cors());
+app.use(express.json());
+
+// Db Name : saadBookDb
+// pass : bFLG1mqlCa4e02su;
+
+const dbUserName = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+console.log(dbUserName, dbPassword);
+// mongoDb import
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const uri = `mongodb+srv://${dbUserName}:${dbPassword}@cluster0.58zpnyp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.get("/blog", (req, res) => {
-  res.send(blogs);
+// mongoDb crud
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db("devsBlogDB");
+    const blogCollection = database.collection("blogs");
+    // const AdminCollection = database.collection("Admin");
+
+    // here all crud oparation
+
+    // post blog data
+    app.post("/blog", async (req, res) => {
+      const blog = req.body;
+      const result = await blogCollection.insertOne(blog);
+      res.send(result);
+      console.log(result);
+    });
+
+    //
+
+    // get  all data
+    app.get("/blog", async (req, res) => {
+      // res.send(blogs);
+      const cusor = blogCollection.find();
+      const result = await cusor.toArray();
+      res.send(result);
+    });
+
+    // get single data
+
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogCollection.findOne(query);
+      res.send(result);
+    });
+    // Delete the first document in  collection
+
+    app.delete("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("delete id ", id);
+      const query = { _id: new ObjectId(id) };
+      const result = await blogCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // update
+
+    app.put("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const blogs = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: true };
+      const updateblogs = {
+        $set: {
+          title: blogs.title,
+          description: blogs.description,
+          imageLink: blogs.imageLink,
+          category: blogs.category,
+          author: blogs.author,
+        },
+      };
+      const result = await blogCollection.updateOne(
+        filter,
+        updateblogs,
+        option
+      );
+      res.send(result);
+      console.log(result);
+    });
+
+    //  blog role api
+
+    // post blog role
+    app.post("/blogs/admin", async (req, res) => {
+      const blog = req.body;
+      const result = await AdminCollection.insertOne(blog);
+      res.send(result);
+      console.log(result);
+    });
+    // get  blog role
+    app.get("/blogs/admin", async (req, res) => {
+      // res.send(blogs);
+      const cusor = AdminCollection.find();
+      const result = await cusor.toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Devs-Blog  Database");
 });
 
 app.listen(port, () => {
